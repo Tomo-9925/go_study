b53ff487eb5c74b4c2cd1f267a00079519c38f62
masi_container <masi19bw@gmail.com>
2020-05-24 05:11:52 +0000

    Implements CheckSrcPort function

--- pkg/utility/packet.go
+++ pkg/utility/packet.go
@@ -2,17 +2,19 @@ package utility
 
 import (
 	gnq "github.com/AkihiroSuda/go-netfilter-queue"
+	"github.com/google/gopacket/layers"
 )
 
-//CheckProtocol return Protocol TCP:1 UDP:2 Others:0
-func CheckProtocol(packet gnq.NFPacket) int {
-	protocol := uint16(packet.Packet.Layers()[0].LayerContents()[9])
-	switch protocol {
-	case 6:
-		return 1
-	case 16:
-		return 2
-	default:
-		return 0
-	}
+//CheckSrcPort returns source port 
+func CheckSrcPort(p gnq.NFPacket) int {
+if tcpLayer := p.Packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+  // Get actual TCP data from this layer
+	tcp, _ := tcpLayer.(*layers.TCP)
+	return (int)(tcp.SrcPort)
+}else if udpLayer :=p.Packet.Layer(layers.LayerTypeUDP); udpLayer !=nil {
+	// Get actual UDP data from this layer
+	udp,_:=udpLayer.(*layers.UDP)
+	return (int)(udp.SrcPort)
+}
+return -1
 }
